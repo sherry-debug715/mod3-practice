06319a2e3974fdb89066658c760fef1d127d0a40
sherryyu <bambooyu715@gmail.com>
2023-08-08 13:10:42 -0400

    updating short practice

--- week8/day2/shorts/nodelist-vs-htmlcollection/assets/js/html-collection.js
+++ week8/day2/shorts/nodelist-vs-htmlcollection/assets/js/html-collection.js
@@ -1,11 +1,15 @@
 export default () => {
   const bodyChildElements = document.body.children; // HTMLCollection [div]
-  // console.log(bodyChildElements);
+  console.log(bodyChildElements);
 
   const div = bodyChildElements[0];
 
   const divChildElements = div.children; // HTMLCollection [span]
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+  setTimeout(() => {
+    div.innerText = "Change the world"
+  },[1000]);
+
   const span = divChildElements[0]; // <span>Yes!</span>
   // debugger
 };
